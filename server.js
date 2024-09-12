9943cf737dbb2602e38b65c881deb98d03911771
Jeremiah Seeley <jeremiah@nativeframe.com>
2024-09-12 09:55:54 -0500

    advanced-webhook-integration; add simple example responses

---
+++ server.js
@@ -0,0 +1,115 @@
+// Import the express module
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const names = [
+	'James',
+	'John',
+	'Robert',
+	'Michael',
+	'William',
+	'David',
+	'Richard',
+];
+
+module.exports.getRandom = function getRandom() {
+	return names[Math.floor(Math.random() * names.length)];
+};
+
+// Create an instance of express
+const app = express();
+
+// Define a port
+const PORT = process.env.PORT || 3000;
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const broadcastBegin = async (req, res) => {
+	console.log('integration broadcastBegin', {
+    privateKey: req.params.privateKey
+  });
+	const resp = {
+		broadcasterProfile: "", // optional profile that customizes transcoder settings
+		needsAuth: false, // whether the broadcast needs to authenticate for the playback
+		publicKey: uuidv4(), // public key for the broadcast
+		userSlug: `user${names.getRandom()}${Math.floor(Math.random() * 10)}`, // optional unique username for the broadcast
+	};
+	res.status(200).send(resp);
+};
+
+const broadcastEnd = async (req, res) => {
+  console.log('integration broadcastEnd', {
+    privateKey: req.params.privateKey
+  });
+	const resp = {
+		status: 'OK',
+		message: 'Broadcast removed',
+	};
+	res.status(200).send(resp);
+};
+
+const formatBegin = async (req, res) => {
+  console.log('integration formatBegin', {
+    privateKey: req.params.privateKey,
+    encoding: req.params.encoding
+  });
+	const resp = {
+		status: 'OK',
+		message: 'Broadcast can continue',
+	};
+	res.status(200).send(resp);
+};
+
+const formatEnd = async (req, res) => {
+  console.log('integration formatEnd', {
+    privateKey: req.params.privateKey,
+    encoding: req.params.encoding
+  });
+	const resp = {
+		status: 'OK',
+		message: 'Encoding removed',
+	};
+	res.status(200).send(resp);
+};
+
+const bulkPing = async (req, res) => {
+  console.log('integration bulkPing', {
+    privateKeys: req.body.privateKeys
+  });
+	const resp = {};
+	// this simulates a "good" response for each key
+	req.body.privateKeys.forEach((privateKey) => {
+		resp[privateKey] = {
+			statusCode: 200,
+			status: 'OK',
+			message: 'Broadcast can continue',
+			needsAuth: false, // whether the broadcast needs to authenticate for the playback
+			broadcasterProfile: "", // optional profile for transcoding profile
+		};
+	});
+	return res.status(200).send(resp);
+};
+
+// called on broadcast start; ensure the privateKey is allowed to begin
+app.put('/apps/demos/api/v1/broadcast/:privateKey', broadcastBegin);
+
+// called on broadcast end; mark stream as completed
+app.delete('/apps/demos/api/v1/broadcast/:privateKey', broadcastEnd);
+
+// called when a format/variant becomes available
+app.put('/apps/demos/api/v1/broadcast/:privateKey/encoding/:encoding', formatBegin);
+
+// called when a format/variant becomes unavailable
+app.delete('/apps/demos/api/v1/broadcast/:privateKey/encoding/:encoding', formatEnd);
+
+// called periodically to check if broadcasts have changed state
+app.put('/apps/demos/api/v1/broadcast/ping/bulk', bulkPing);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
