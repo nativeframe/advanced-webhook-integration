f6e4bd1a338bf79695ca131545b2f1c0df07b48e
Jeremiah Seeley <jeremiah@nativeframe.com>
2025-11-17 14:07:01 -0600

    add 'bulkvalidate' route

---
+++ apiReference.md
@@ -0,0 +1,187 @@
+
+# Broadcast API Documentation
+
+## Broadcast Begin
+
+Called on broadcast start; Ensures a broadcast is allowed to begin by `privateKey`
+
+```js
+PUT - {host}/integration/v1/broadcast/{privateKey}
+
+// Request payload:
+{
+  clientIP: '0.0.0.0',           // IP of the broadcaster
+  clientAgent: 'clientAgent',    // Client agent
+  clientReferrer: 'staging',     // Client referrer (aka: project name)
+  clientEncoder: 'FMLE',         // Description of the broadcast encoder being used
+  originURL: 'rtmp://',          // URL for playback from the origin
+  manifest: 'http://',           // URL for viewer playback
+}
+
+// Response: 200
+{
+  userSlug: 'userHenry82',       // Random user name assigned by demo-api
+  broadcasterProfile: 'high',    // Profile name to use for transcoding (optional)
+  needsAuth: true,               // Authorization state (indicates if authorization is required for playback)
+  publicKey: 'foo',              // The key to use when calling for the manifest for viewer playback
+}
+
+```
+
+## Broadcast End
+
+Called on broadcast end; Mark stream as completed by `privateKey`
+
+```js
+DELETE - {host}/integration/v1/broadcast/{privateKey}
+
+// Response: 200
+{
+  status: 'OK',
+  message: 'Broadcast removed',
+}
+
+```
+
+## Format Begin
+
+Called when a `format` encoding (aka: variants) becomes available
+
+```js
+PUT - {host}/v1/integration/broadcast/{streamKey}/encoding/{format}
+// Note: streamKey is sometimes referred to as privateKey
+
+// Request payload:
+{
+  publicKey: 'foo',              // The key to use when calling for the manifest for viewer playback
+  videoCodec: 'mp4',             // Video codec for the format
+  audioCodec: 'aac',             // Audio codec for the format
+  manifest: 'http://',           // URL for viewer playback
+  location: 'http://',           // URL to directly playback or pull this format for internal use
+  clientEncoder: 'FMLE',         // Description of the broadcast encoder being used
+  encodings: [
+    {
+      isOriginal: true,          // Indication if this is the origin stream (not an encoded variant)
+      videoWidth: 640,           // Video resolution width
+      videoHeight: 360,          // Video resolution height
+      videoKbps: 1200,           // Video streaming rate in kbps
+      audioKbps: 96,             // Audio streaming rate in kbps
+      videoPts: 12478,           // Current video presentation timestamp
+      audioPts: 12478,           // Current audio presentation timestamp
+      collected: 12478,          // Timestamp stats were collected
+      location: 'http://',       // URL to directly playback or pull this format for internal use
+    }
+  ]
+}
+
+// Response: 200
+{
+  status: 'OK',
+  message: 'Broadcast can continue',
+}
+
+```
+
+## Format End
+
+Called when a `format` encoding (aka: variants) becomes unavailable
+
+```js
+DELETE - {host}/integration/v1/broadcast/{streamKey}/encoding/{format}
+// Note: streamKey is sometimes referred to as privateKey
+
+// Response: 200
+{
+  status: 'OK',
+  message: 'Encoding removed',
+}
+
+```
+
+## Bulk Ping
+
+Called periodically to check if broadcasts have changed state by `privateKey`
+
+```js
+PUT - {host}/integration/v1/broadcast/ping/bulk
+
+// Note: This route can be used to check for multiple streams at the same time
+// by passing an array of privateKeys.
+
+// Request payload:
+{
+  privateKeys: ['foo', 'bar']
+}
+
+// Response: 200
+{
+  foo: {
+    statusCode: 200,             // Status code for individual stream (privateKey 'foo')
+    status: 'OK',
+    message: 'Broadcast can continue',
+    needsAuth: true,             // Authorization state (indicates if authorization is required for playback)
+    broadcasterProfile: 'high'   // Profile name to use for transcoding (optional)
+  },
+  bar: {
+	statusCode: 403,             // Status code for individual stream (privateKey 'bar')
+    status: 'STOP',
+    message: 'Amin called stop',
+    needsAuth: true,             // Authorization state (indicates if authorization is required for playback)
+  }
+}
+
+```
+
+## Bulk Validate (Viewer Authorization)
+
+Called by the auth service to check access for multiple `privateKeys` and `accessTokens` by sending a hash of privateKeys mapping to an array of tokens to check
+
+```js
+POST - {host}/integration/v1/broadcast/token/bulkvalidate
+
+// Request payload:
+{
+  privateKeyFoo: ['tokenBar', 'tokenBaz'],
+  privateKeyQux: ['tokenFred'],
+  privateKeyThud: ['tokenFizz', 'tokenBuzz'],
+}
+
+// Response: 200
+{
+  privateKeyFoo: {
+    tokenBar: {
+      statusCode: 200,
+      message: 'OK',
+      status: true,
+      ttl: 3600,                 // TTL may or may not be returned
+    },
+    tokenBaz: {
+      statusCode: 403,
+      message: 'Room inaccessible',
+      status: false,
+    },
+  },
+  privateKeyQux: {
+    tokenFred: {
+      statusCode: 200,
+      message: 'OK',
+      status: true,
+      ttl: 3600,                 // TTL may or may not be returned
+    },
+  },
+  privateKeyThud: {
+    tokenFizz: {
+      statusCode: 200,
+      message: 'OK',
+      status: true,
+      ttl: 3600,                 // TTL may or may not be returned
+    },
+    tokenBuzz: {
+      statusCode: 403,
+      message: 'Room inaccessible',
+      status: false,
+    }
+  },
+}
+
+```
--- server.js
+++ server.js
@@ -1,102 +1,130 @@
 // Description: Example integration server for broadcast webhooks
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 // list of example names
 const names = [
-	'James',
-	'John',
-	'Robert',
-	'Michael',
-	'William',
-	'David',
-	'Richard',
+  'James',
+  'John',
+  'Robert',
+  'Michael',
+  'William',
+  'David',
+  'Richard',
 ];
 
 // Get a random name
 const getRandomName = function getRandom() {
-	return names[Math.floor(Math.random() * names.length)];
+  return names[Math.floor(Math.random() * names.length)];
 };
 
 const broadcastBegin = async (req, res) => {
-	console.log('integration broadcastBegin', {
-    privateKey: req.params.privateKey
+  console.log('integration broadcastBegin', {
+	privateKey: req.params.privateKey,
   });
-	const resp = {
-		broadcasterProfile: null, // optional profile name that customizes transcoder settings
-		needsAuth: false, // set true if the broadcast needs authentication for viewer playback
-		publicKey: uuidv4(), // public key for the broadcast viewer playback
-		userSlug: `user${getRandomName()}${Math.floor(Math.random() * 10)}`, // optional unique username for the broadcast
-	};
-	res.status(200).send(resp);
+  const resp = {
+	broadcasterProfile: null, // optional profile name that customizes transcoder settings
+	needsAuth: false, // set true if the broadcast needs authentication for viewer playback
+	publicKey: uuidv4(), // public key for the broadcast viewer playback
+	userSlug: `user${getRandomName()}${Math.floor(Math.random() * 10)}`, // optional unique username for the broadcast
+  };
+  res.status(200).send(resp);
 };
 
 const broadcastEnd = async (req, res) => {
   console.log('integration broadcastEnd', {
-    privateKey: req.params.privateKey
+	privateKey: req.params.privateKey,
   });
-	const resp = {
-		status: 'OK',
-		message: 'Broadcast removed',
-	};
-	res.status(200).send(resp);
+  const resp = {
+	status: 'OK',
+	message: 'Broadcast removed',
+  };
+  res.status(200).send(resp);
 };
 
 const formatBegin = async (req, res) => {
   console.log('integration formatBegin', {
-    privateKey: req.params.privateKey,
-    encoding: req.params.encoding
+	privateKey: req.params.privateKey,
+	encoding: req.params.encoding,
   });
-	const resp = {
-		status: 'OK',
-		message: 'Broadcast can continue',
-	};
-	res.status(200).send(resp);
+  const resp = {
+	status: 'OK',
+	message: 'Broadcast can continue',
+  };
+  res.status(200).send(resp);
 };
 
 const formatEnd = async (req, res) => {
   console.log('integration formatEnd', {
-    privateKey: req.params.privateKey,
-    encoding: req.params.encoding
+	privateKey: req.params.privateKey,
+	encoding: req.params.encoding,
   });
-	const resp = {
-		status: 'OK',
-		message: 'Encoding removed',
-	};
-	res.status(200).send(resp);
+  const resp = {
+	status: 'OK',
+	message: 'Encoding removed',
+  };
+  res.status(200).send(resp);
 };
 
 const bulkPing = async (req, res) => {
   console.log('integration bulkPing', {
-    privateKeys: req.body.privateKeys
+	privateKeys: req.body.privateKeys,
   });
-	const resp = {};
-	// this simulates a "good" response for each key
-	req.body.privateKeys.forEach((privateKey) => {
-		resp[privateKey] = {
-			statusCode: 200,
-			status: 'OK',
-			message: 'Broadcast can continue',
-			needsAuth: false, // set true if the broadcast needs authentication for viewer playback
-			broadcasterProfile: null, // optional profile name that customizes transcoder settings
-		};
+  const resp = {};
+  // this simulates a "good" response for each key
+  (req.body.privateKeys || []).forEach((privateKey) => {
+	resp[privateKey] = {
+	  statusCode: 200,
+	  status: 'OK',
+	  message: 'Broadcast can continue',
+	  needsAuth: false, // set true if the broadcast needs authentication for viewer playback
+	  broadcasterProfile: null, // optional profile name that customizes transcoder settings
+	};
+  });
+  return res.status(200).send(resp);
+};
+
+const tokenBulkValidate = async (req, res) => {
+  console.log('integration tokenBulkValidate', {
+	body: req.body,
+  });
+  const resp = {};
+  const ttlSeconds = 2 * 60; // token is valid for 2 minutes
+  const privateKeys = req.body ? Object.keys(req.body) : [];
+  privateKeys.forEach((privateKey) => {
+	// create a response entry for each privateKey
+	resp[privateKey] = {};
+	// ensure there is an array of tokens to validate
+	const tokens = Array.isArray(req.body[privateKey]) ? req.body[privateKey] : [];
+	tokens.forEach((token) => {
+	  // insert an entry in the privateKey object for each token
+	  resp[privateKey][token] = {
+		statusCode: 200, // 200 the token is valid, 403 the token is invalid
+		status: true,
+		message: 'OK',
+		ttl: ttlSeconds,
+	  };
 	});
-	return res.status(200).send(resp);
+  });
+  return res.status(200).send(resp);
 };
 
 // Create express instance
 const app = express();
 
+// allow parsing JSON bodies
+app.use(express.json());
+
 // Define express port
 const PORT = process.env.PORT || 3000;
 
-// Called on broadcast start; Ensure the privateKey is allowed to begin
+// Called on broadcast start; Ensures a privateKey is allowed to begin a broadcast
 app.put('/integration/v1/broadcast/:privateKey', broadcastBegin);
 
 // Called on broadcast end; Mark broadcast as completed
 app.delete('/integration/v1/broadcast/:privateKey', broadcastEnd);
 
-// Called when a transcoding variant becomes available; Indetifies that viewer manifest is ready
+// Called when a transcoding variant becomes available; Identifies that a player manifest is ready
 app.put('/integration/v1/broadcast/:privateKey/encoding/:encoding', formatBegin);
 
 // Called when a transcoding variant becomes unavailable
@@ -104,6 +132,9 @@ app.delete('/integration/v1/broadcast/:privateKey/encoding/:encoding', formatEnd
 
 // Called periodically to check if broadcasts have changed state
 app.put('/integration/v1/broadcast/ping/bulk', bulkPing);
+
+// Called when authentication is required for viewer playback
+app.post('/integration/v1/broadcast/token/bulkvalidate', tokenBulkValidate);
 
 // Start the server
 app.listen(PORT, () => {
